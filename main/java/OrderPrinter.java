cc9e7f1b4efdb5bba9da2ac860f070c290dc0d54
Navid Ibrahim <nibrahim7787@gmail.com>
2024-10-07 09:01:39 +0600

    Added OrderPrinter

--- main/java/OrderPrinter.java
+++ main/java/OrderPrinter.java
@@ -1,2 +1,15 @@
+import java.util.Comparator;
+import java.util.List;
+
 public class OrderPrinter {
+    public void printShapesInOrder(List<Shape> shapes) {
+        shapes.sort(Comparator.comparing(Shape::calculateArea).thenComparing(Shape::getX).thenComparing(Shape::getY));
+
+        for (Shape shape : shapes) {
+            System.out.println("Shape: " + shape.getClass().getSimpleName());
+            System.out.println("Area: " + shape.calculateArea());
+            System.out.println("Position: " + shape.getX() + ", " + shape.getY());
+            System.out.println("\n\n");
+        }
+    }
 }
