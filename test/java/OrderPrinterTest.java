b79914cbf86880b8a68be83b874d9b316b303111
Navid Ibrahim <nibrahim7787@gmail.com>
2024-10-07 09:58:55 +0600

    Refactored and added 1 test case

--- test/java/OrderPrinterTest.java
+++ test/java/OrderPrinterTest.java
@@ -7,47 +7,54 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class OrderPrinterTest {
     @Test
-    public void areaOrderTest() {
-        List<Shape> shapes = new ArrayList<>();
-        shapes.add(new Circle(1, 1, 2));
-        shapes.add(new Square(0, 0, 3));
-        shapes.add(new Rectangle(2, 2, 2, 5));
+    public void generalAreaOrderTest() {
+        List<Shape> shapeList = new ArrayList<>();
+        shapeList.add(new Circle(1, 1, 2));
+        shapeList.add(new Square(0, 0, 3));
+        shapeList.add(new Rectangle(2, 2, 2, 5));
 
         OrderPrinter printer = new OrderPrinter();
-        printer.printShapesInOrder(shapes);
+        printer.printShapesInOrder(shapeList);
 
-        assertTrue(shapes.get(0) instanceof Square);
-        assertTrue(shapes.get(1) instanceof Rectangle);
-        assertTrue(shapes.get(2) instanceof Circle);
+        assertTrue(shapeList.get(0).calculateArea() < shapeList.get(1).calculateArea());
+        assertTrue(shapeList.get(1).calculateArea() < shapeList.get(2).calculateArea());
+    }
+
+    @Test
+    public void notEmptyOrderTest() {
+        List<Shape> shapeList = new ArrayList<>();
+        Shape shape = new Square(5, 6, 10);
+        shapeList.add(shape);
+        assertNotNull(shapeList.get(0));
     }
 
     @Test
     public void areaByXOrderTest() {
-        List<Shape> shapes = new ArrayList<>();
-        shapes.add(new Square(3, 1, 4));
-        shapes.add(new Square(1, 2, 4));
-        shapes.add(new Square(2, 3, 4));
+        List<Shape> shapeList = new ArrayList<>();
+        shapeList.add(new Square(3, 1, 4));
+        shapeList.add(new Square(1, 2, 4));
+        shapeList.add(new Square(2, 3, 4));
 
         OrderPrinter printer = new OrderPrinter();
-        printer.printShapesInOrder(shapes);
+        printer.printShapesInOrder(shapeList);
 
-        assertEquals(1, shapes.get(0).getX());
-        assertEquals(2, shapes.get(1).getX());
-        assertEquals(3, shapes.get(2).getX());
+        assertEquals(1, shapeList.get(0).getX());
+        assertEquals(2, shapeList.get(1).getX());
+        assertEquals(3, shapeList.get(2).getX());
     }
 
     @Test
     public void areaByYOrderTest() {
-        List<Shape> shapes = new ArrayList<>();
-        shapes.add(new Square(1, 5, 4));
-        shapes.add(new Square(1, 3, 4));
-        shapes.add(new Square(1, 1, 4));
+        List<Shape> shapeList = new ArrayList<>();
+        shapeList.add(new Square(1, 5, 4));
+        shapeList.add(new Square(1, 3, 4));
+        shapeList.add(new Square(1, 1, 4));
 
         OrderPrinter printer = new OrderPrinter();
-        printer.printShapesInOrder(shapes);
+        printer.printShapesInOrder(shapeList);
 
-        assertEquals(1, shapes.get(0).getY());
-        assertEquals(3, shapes.get(1).getY());
-        assertEquals(5, shapes.get(2).getY());
+        assertEquals(1, shapeList.get(0).getY());
+        assertEquals(3, shapeList.get(1).getY());
+        assertEquals(5, shapeList.get(2).getY());
     }
 }
